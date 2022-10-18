fb03efd13fb6e1ed0eca24db521e21f6f695623c
Aditya Painuly <adivp7@gmail.com>
2022-10-18 20:22:10 +0530

    change

--- question_dict.py
+++ question_dict.py
@@ -13,19 +13,20 @@ def main():
     for ques in ques_data['stat_status_pairs']:
         if not ques['paid_only']:
             ques_dict[ques['stat']['frontend_question_id']] = {
+                'ques_id': ques['stat']['frontend_question_id'],
                 'question_title': ques['stat']['question__title'],
                 'question_title_slug': ques['stat']['question__title_slug'],
                 'difficulty_level': ques['difficulty']['level']
             }
-
-    ques_dict = SortedDict(ques_dict)
     # pprint(ques_dict)
+
+    ques_dict=SortedDict(ques_dict)
 
     with open('question_details.pkl', 'wb') as filehandler:
         pickle.dump(ques_dict, filehandler)
-
-    with open("ques.json", "w") as filehandler:
-        json.dump(ques_dict, filehandler)
+    
+    with open("ques.json","w") as fjson:
+        json.dump(ques_dict,fjson)
 
 
 if __name__ == "__main__":
--- scraping.py
+++ scraping.py
@@ -1,11 +1,9 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 import pickle
 from pprint import pprint
 from bs4 import BeautifulSoup
 import json
+import time
 
 def main():
     with open('question_details.pkl','rb') as filehandler: 
@@ -15,22 +13,21 @@ def main():
     browser=webdriver.Chrome()
     # browser.implicitly_wait(30)
 
-    cnt=1
+    cnt=0
     for ques_no in ques_dict:
-        if cnt==50:
+        ques_slug=ques_dict[ques_no]['question_title_slug']
+        browser.get("https://leetcode.com/problems/"+ques_slug)
+        soup=BeautifulSoup(browser.page_source,'html5lib')
+        desc=soup.find("div", {"class": "content__u3I1 question-content__JfgR"})
+        ques_dict[ques_no]['ques_id']=ques_no
+        ques_dict[ques_no]['description_html']=str(desc)
+        with open(f"ques/{ques_no}.json", "w") as filehandler:
+            json.dump(ques_dict[ques_no], filehandler)
+        cnt+=1
+        if cnt%15==0:
+            time.sleep(60)
+        if cnt==100:
             break
-        try:
-            ques_slug=ques_dict[ques_no]['question_title_slug']
-            browser.get("https://leetcode.com/problems/"+ques_slug)
-            soup=BeautifulSoup(browser.page_source,'html5lib')
-            desc=soup.find("div", {"class": "content__u3I1 question-content__JfgR"})
-            ques_dict[ques_no]['description_html']=str(desc)
-            with open(f"ques{ques_no}.json", "w") as filehandler:
-                json.dump(ques_dict[ques_no], filehandler)
-            cnt+=1
-        except:
-            print(f"failed: {ques_slug=}") #type: ignore
-            input()
 
     browser.quit()
 
